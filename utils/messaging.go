8610a1963d45c2d98c5d6cf85a27d27f16d86ee2
Roope <roope.hongisto@gritlab.ax>
2024-12-17 12:49:41 +0200

    added broadcaster

--- utils/messaging.go
+++ utils/messaging.go
@@ -9,27 +9,6 @@ import (
 	"time"
 )
 
-// broadcastMessages stores and sends messages to all clients from the message buffer
-func BroadcastMessages() {
-	for msg := range messageBuffer { // messageBuffer is a channel so for loop doesn't exit
-		messageMutex.Lock()
-		messages = append(messages, msg)
-		messageMutex.Unlock()
-
-		clientMutex.Lock()
-		for client := range clients {
-			WriteToClient(msg, client.Writer, true) // write the messsage into the buffer managed by the bufio.Writer
-			client.Writer.Flush()                   // force write the buffered data to the underlying connection
-		}
-		clientMutex.Unlock()
-	}
-}
-
-// broadcast sends the string to the messageBuffer channel
-func broadcast(msg string) {
-	messageBuffer <- msg
-}
-
 // sendHistory sends all the previous messages to a clients writer
 func sendHistory(writer *bufio.Writer) {
 	messageMutex.RLock()
