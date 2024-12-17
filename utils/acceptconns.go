8cdbcc036bbc47fa07399104827efc60511433e2
Roope Hongisto <roope@Roopes-MacBook-Air.local>
2024-12-17 18:36:54 +0200

    added some comments

--- utils/acceptconns.go
+++ utils/acceptconns.go
@@ -1,20 +1,17 @@
 package utils
 
 import (
-	"errors"
 	"fmt"
 	"net"
 	"sync"
 )
-
+// listens for incoming client connections on the given listener.
 func AcceptConnections(shutdown chan struct{}, listener net.Listener, wg *sync.WaitGroup) {
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			if !errors.Is(err, net.ErrClosed) {
-				fmt.Printf("Error accepting connection: %v\n", err)
-			}
-			continue
+			fmt.Printf("Error accepting connection: %v\n", err)
+			return // Exit the loop on any error (including listner close)
 		}
 		wg.Add(1)
 		go HandleClient(conn, shutdown, wg)
